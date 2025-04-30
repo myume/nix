8c7ad752c400b095b12c4ccd48399a768adc90a2
myu <contact@memyu.com>
2025-04-30 18:00:30 -0400

    feat(nvim/langauges): add language support

--- home/myu/programs/terminal/nvim/plugins/languages.nix
+++ home/myu/programs/terminal/nvim/plugins/languages.nix
@@ -1,11 +1,26 @@
 {
   programs.nvf.settings.vim = {
     languages = {
-      nix = {
-        enable = true;
-        format = {enable = true;};
-        lsp = {enable = true;};
-      };
+      enableLSP = true;
+      enableFormat = true;
+      enableTreesitter = true;
+      enableExtraDiagnostics = true;
+
+      bash.enable = true;
+      css.enable = true;
+      clang.enable = true;
+      go.enable = true;
+      haskell.enable = true;
+      html.enable = true;
+      lua.enable = true;
+      markdown.enable = true;
+      nix.enable = true;
+      python.enable = true;
+      rust.enable = true;
+      sql.enable = true;
+      tailwind.enable = true;
+      ts.enable = true;
+      zig.enable = true;
     };
   };
 }
