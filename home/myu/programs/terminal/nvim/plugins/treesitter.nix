50e07841f4f44233fba52935ce4da4aaf1a56172
myu <contact@memyu.com>
2025-05-01 13:34:41 -0400

    fix(nvim): disable treesitter context

--- home/myu/programs/terminal/nvim/plugins/treesitter.nix
+++ home/myu/programs/terminal/nvim/plugins/treesitter.nix
@@ -1,7 +1,6 @@
 {
   programs.nvf.settings.vim = {
     treesitter = {
-      context.enable = true;
       fold = true;
       autotagHtml = true;
       indent.enable = false; # wonky for some reason
