00f72988d738efac5f565f1c3eec43240451bde2
myu <contact@memyu.com>
2025-04-30 23:27:48 -0400

    feat(nvim/dashboard): remove bloat and keep minimal

--- home/myu/programs/terminal/nvim/plugins/dashboard.nix
+++ home/myu/programs/terminal/nvim/plugins/dashboard.nix
@@ -2,6 +2,15 @@
   programs.nvf.settings.vim = {
     dashboard.dashboard-nvim = {
       enable = true;
+      setupOpts = {
+        theme = "hyper";
+        config = {
+          week_header.enable = true;
+          packages.enable = false;
+          shortcut = {};
+          footer = {};
+        };
+      };
     };
   };
 }
