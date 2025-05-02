b5d009ae7fa766e5367bbcda71eeb218601e1499
myu <contact@memyu.com>
2025-05-02 18:53:25 -0400

    feat(git): add better diffs and logs for git

--- home/myu/programs/terminal/tools/git.nix
+++ home/myu/programs/terminal/tools/git.nix
@@ -4,5 +4,11 @@
     userName = "myu";
     userEmail = "contact@memyu.com";
     extraConfig = {init.defaultBranch = "main";};
+
+    difftastic.enable = true;
+
+    aliases = {
+      lg = "log --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'";
+    };
   };
 }
