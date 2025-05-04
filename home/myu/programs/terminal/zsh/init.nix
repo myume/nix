249af0e49ca920dca9860e8151634705d84a9d8a
myu <contact@memyu.com>
2025-05-04 13:52:58 -0400

    misc(zsh): add ctrl+[k/j] to history substring search

--- home/myu/programs/terminal/zsh/init.nix
+++ home/myu/programs/terminal/zsh/init.nix
@@ -3,6 +3,15 @@
     bindkey "^ " autosuggest-accept
     bindkey "^H" backward-kill-word
 
+    function init() {
+      bindkey "^K" history-substring-search-up
+      bindkey -M vicmd "^K" history-substring-search-up
+      bindkey "^J" history-substring-search-down
+      bindkey -M vicmd "^J" history-substring-search-down
+    }
+
+    zvm_after_init_commands+=(init)
+
     function zvm_after_lazy_keybindings() {
       bindkey -M vicmd "L" end-of-line
       bindkey -M vicmd "H" beginning-of-line
--- home/myu/programs/terminal/zsh/variables.nix
+++ home/myu/programs/terminal/zsh/variables.nix
@@ -2,11 +2,17 @@
   programs.zsh = {
     sessionVariables = {
       MANPAGER = "nvim -c 'set ft=man' +Man! -o -";
+
+      # zsh-vi-mode
       ZVM_VI_INSERT_ESCAPE_BINDKEY = "kj";
       ZVM_VI_HIGHLIGHT_BACKGROUND = "#A6ADC8";
       ZVM_VI_HIGHLIGHT_FOREGROUND = "#1E1E2E";
       ZVM_VI_HIGHLIGHT_EXTRASTYLE = "bold";
       ZVM_INSERT_MODE_CURSOR = "$ZVM_CURSOR_BLINKING_BEAM";
+
+      # history substring search
+      HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = true;
+      HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND = "bg=black,fg=white,bold";
     };
   };
 }
