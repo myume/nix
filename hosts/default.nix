6d0fb666e475a0edd8456e382bec0e5dc7ad23ae
myu <contact@memyu.com>
2025-05-03 23:33:41 -0400

    refactor: modularize hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,22 +3,32 @@
   home-manager,
   ...
 } @ inputs: {
-  navi = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
+  navi = let
+    hostname = "navi";
+  in
+    nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit hostname;
+      };
 
-    modules = [
-      ./navi
+      modules = [
+        (./. + "/${hostname}")
 
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
 
-        home-manager.extraSpecialArgs = {inherit inputs;};
-        home-manager.users = {
-          myu = import ../home/myu/home.nix;
-        };
-      }
-    ];
-  };
+            extraSpecialArgs = {
+              inherit inputs;
+            };
+            users = {
+              myu = import ../home/myu/home.nix;
+            };
+          };
+        }
+      ];
+    };
 }
--- hosts/navi/default.nix
+++ hosts/navi/default.nix
@@ -1,10 +1,9 @@
 {pkgs, ...}: let
   modPath = ../../modules;
-  progPath = modPath + /programs;
 
   securityImports =
     builtins.map
-    (module: modPath + /security + ("/" + module + ".nix"))
+    (module: "${modPath}/security/${module}.nix")
     [
       "fprint"
       "pam"
@@ -14,38 +13,13 @@
 in {
   imports =
     [
+      "${modPath}/programs"
+      "${modPath}/networking"
+      "${modPath}/locale"
+
       ./hardware-configuration.nix
-      progPath
     ]
     ++ securityImports;
-
-  networking.hostName = "navi"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   boot = {
     # Track the latest Linux kernel release for improved hardware support
