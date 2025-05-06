258e1297543424a7eb8bd98deb2b8b1d72178c6d
myu <contact@memyu.com>
2025-05-05 23:43:44 -0400

    misc: remove redundant gc in zsh alias

--- home/myu/programs/terminal/zsh/aliases.nix
+++ home/myu/programs/terminal/zsh/aliases.nix
@@ -1,7 +1,7 @@
 {
   programs.zsh.shellAliases = {
     rebuild = "sudo nixos-rebuild switch";
-    cleanup = "sudo nix-collect-garbage --delete-older-than 3d && sudo nix-collect-garbage -d";
+    gc = "sudo nix-collect-garbage -d";
 
     cat = "bat";
     grep = "rg";
