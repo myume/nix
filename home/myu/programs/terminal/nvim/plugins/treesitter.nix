461cadfd364b94a12e47f12d859f463be067fd1c
myu <contact@memyu.com>
2025-05-06 13:51:17 -0400

    fix(nvim): manually enable typescript treesitter

--- home/myu/programs/terminal/nvim/plugins/treesitter.nix
+++ home/myu/programs/terminal/nvim/plugins/treesitter.nix
@@ -1,9 +1,14 @@
-{
+{pkgs, ...}: {
   programs.nvf.settings.vim = {
     treesitter = {
+      enable = true;
       fold = true;
       autotagHtml = true;
       indent.enable = false; # wonky for some reason
+
+      grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        typescript # in language settings only tsx gets enabled, not typescript
+      ];
     };
   };
 }
