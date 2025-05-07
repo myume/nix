26ed78fec4f62951ed4df9841b0f43ade00e7a79
myu <contact@memyu.com>
2025-05-06 22:14:31 -0400

    feat(nvim): use smartcase

--- home/myu/programs/terminal/nvim/default.nix
+++ home/myu/programs/terminal/nvim/default.nix
@@ -23,6 +23,7 @@
       vim = {
         viAlias = true;
         vimAlias = true;
+        searchCase = "smart";
         options = {
           cursorline = true;
           cursorlineopt = "number";
