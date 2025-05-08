5f24c151659509e868f2aa87243b577c63dbb8b2
myu <contact@memyu.com>
2025-05-08 00:16:58 -0400

    fix(nvim) fix bad indentation by treesitter
    needed to use guess indent. will indent by default amount (8)

--- home/myu/programs/terminal/nvim/plugins/treesitter.nix
+++ home/myu/programs/terminal/nvim/plugins/treesitter.nix
@@ -4,7 +4,7 @@
       enable = true;
       fold = true;
       autotagHtml = true;
-      indent.enable = false; # wonky for some reason
+      indent.enable = true;
 
       grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
         typescript # in language settings only tsx gets enabled, not typescript
