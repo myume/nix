019f10456793af0aec8858f46d6e2e5ed5ca6479
myu <contact@memyu.com>
2025-05-09 00:05:12 -0400

    feat(gtk): add gtk themes

--- home/myu/programs/wayland/hyprland/settings/exec.nix
+++ home/myu/programs/wayland/hyprland/settings/exec.nix
@@ -1,7 +1,10 @@
-{
+let
+  cursorName = "Bibata-Original-Ice";
+in {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "hyprlock --immediate-render --no-fade-in"
+      "hyprctl setcursor ${cursorName} ${toString 16}"
     ];
   };
 }
