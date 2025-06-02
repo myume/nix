281ce179433723fb8441f7bbb13a60845626553c
myu <contact@memyu.com>
2025-06-02 14:02:57 -0400

    feat(services/ags): turn ags into a service

--- home/myu/services/ags/default.nix
+++ home/myu/services/ags/default.nix
@@ -4,6 +4,24 @@
   ...
 }: let
   agsPkgs = inputs.ags.packages.${pkgs.system};
+
+  agsLibs = with agsPkgs; [
+    apps
+    battery
+    bluetooth
+    cava
+    hyprland
+    mpris
+    network
+    notifd
+    powerprofiles
+    tray
+    wireplumber
+  ];
+  # Create AGS package with extra modules
+  agsWithExtras = agsPkgs.default.override {
+    extraPackages = agsLibs;
+  };
 in {
   # add the home manager module
   imports = [inputs.ags.homeManagerModules.default];
@@ -18,20 +36,25 @@ in {
 
     # symlink to ~/.config/ags
     configDir = ./src;
-    extraPackages = with agsPkgs; [
-      apps
-      # auth
-      battery
-      bluetooth
-      cava
-      # greet
-      hyprland
-      mpris
-      network
-      notifd
-      powerprofiles
-      tray
-      wireplumber
-    ];
+    extraPackages = agsLibs;
+  };
+
+  # Create systemd user service
+  systemd.user.services.ags = {
+    Unit = {
+      Description = "Aylur's Gtk Shell";
+      PartOf = [
+        "tray.target"
+        "graphical-session.target"
+      ];
+      After = ["graphical-session.target"];
+    };
+
+    Service = {
+      ExecStart = "${agsWithExtras}/bin/ags run --gtk4";
+      Restart = "on-failure";
+    };
+
+    Install.WantedBy = ["graphical-session.target"];
   };
 }
