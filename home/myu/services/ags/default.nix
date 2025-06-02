9b04aa79f6dcb3ae6d1c39068be1b6926058ae67
myu <contact@memyu.com>
2025-06-02 17:54:13 -0400

    fix(ags): move ags autostart from systemd service to hyprland exec-once
    dbus variable was out of sync in the service

--- home/myu/services/ags/default.nix
+++ home/myu/services/ags/default.nix
@@ -4,24 +4,6 @@
   ...
 }: let
   agsPkgs = inputs.ags.packages.${pkgs.system};
-
-  agsLibs = with agsPkgs; [
-    apps
-    battery
-    bluetooth
-    cava
-    hyprland
-    mpris
-    network
-    notifd
-    powerprofiles
-    tray
-    wireplumber
-  ];
-  # Create AGS package with extra modules
-  agsWithExtras = agsPkgs.default.override {
-    extraPackages = agsLibs;
-  };
 in {
   # add the home manager module
   imports = [inputs.ags.homeManagerModules.default];
@@ -36,25 +18,24 @@ in {
 
     # symlink to ~/.config/ags
     configDir = ./src;
-    extraPackages = agsLibs;
+    extraPackages = with agsPkgs; [
+      apps
+      battery
+      bluetooth
+      cava
+      hyprland
+      mpris
+      network
+      notifd
+      powerprofiles
+      tray
+      wireplumber
+    ];
   };
 
-  # Create systemd user service
-  systemd.user.services.ags = {
-    Unit = {
-      Description = "Aylur's Gtk Shell";
-      PartOf = [
-        "tray.target"
-        "graphical-session.target"
-      ];
-      After = ["graphical-session.target"];
-    };
-
-    Service = {
-      ExecStart = "${agsWithExtras}/bin/ags run --gtk4";
-      Restart = "on-failure";
-    };
-
-    Install.WantedBy = ["graphical-session.target"];
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "ags run --gtk4"
+    ];
   };
 }
