54839919397576b4145d3d336ec9d395e1c27913
myu <contact@memyu.com>
2025-06-04 15:24:57 -0400

    feat(ags): color populated workspaces

--- home/myu/services/ags/src/widget/Bar/modules/Workspaces.tsx
+++ home/myu/services/ags/src/widget/Bar/modules/Workspaces.tsx
@@ -1,17 +1,29 @@
 import Hyprland from "gi://AstalHyprland";
-import { bind } from "astal";
+import { bind, derive } from "astal";
 
 export default function Workspaces() {
   const hypr = Hyprland.get_default();
+  const populatedWs = bind(hypr, "workspaces").as((workspaces) =>
+    workspaces.filter((ws) => ws.clients.length > 0).map(({ id }) => id),
+  );
 
   return (
     <box cssClasses={["workspaces"]}>
-      {Array.from({ length: 9 }, (_, i) => i + 1).map((ws) => {
+      {Array.from({ length: 9 }, (_, i) => i + 1).map((wsId) => {
         const fw = bind(hypr, "focusedWorkspace");
+
+        const wsState = derive([populatedWs, fw], (populatedWs, fw) => [
+          wsId === fw.id
+            ? "focused"
+            : populatedWs.includes(wsId)
+              ? "populated"
+              : "empty",
+        ]);
+
         return (
           <button
-            cssClasses={fw.as((fw) => [ws === fw.id ? "focused" : ""])}
-            onClicked={() => hypr.dispatch("workspace", ws.toString())}
+            cssClasses={wsState()}
+            onClicked={() => hypr.dispatch("workspace", wsId.toString())}
           />
         );
       })}
