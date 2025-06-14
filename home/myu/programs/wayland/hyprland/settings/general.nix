a4155d08da1f76c579411c808642aebfc08a55ab
myu <contact@memyu.com>
2025-06-14 01:02:15 -0400

    style: adjust hyprland gaps

--- home/myu/programs/wayland/hyprland/settings/general.nix
+++ home/myu/programs/wayland/hyprland/settings/general.nix
@@ -1,8 +1,8 @@
 {
   wayland.windowManager.hyprland.settings = {
     general = {
-      gaps_in = 5;
-      gaps_out = "5, 10, 10, 10";
+      gaps_in = 4;
+      gaps_out = "6, 8, 8, 8";
       border_size = 2;
       "col.active_border" = "rgba(89b4faee)";
       "col.inactive_border" = "rgba(1e1e2eee)";
