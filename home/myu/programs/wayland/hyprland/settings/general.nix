468ce21a274a76224596a8c770f5863aceffe6da
myu <contact@memyu.com>
2025-06-13 22:54:35 -0400

    style(ags/hyprland): reduce gaps between bar and windows

--- home/myu/programs/wayland/hyprland/settings/general.nix
+++ home/myu/programs/wayland/hyprland/settings/general.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     general = {
       gaps_in = 5;
-      gaps_out = 10;
+      gaps_out = "5, 10, 10, 10";
       border_size = 2;
       "col.active_border" = "rgba(89b4faee)";
       "col.inactive_border" = "rgba(1e1e2eee)";
