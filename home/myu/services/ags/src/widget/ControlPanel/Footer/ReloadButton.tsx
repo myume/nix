4e851289c50cd8be6a72e7f6e303b1197e3e68fb
myu <contact@memyu.com>
2025-06-22 01:13:55 -0400

    fix: close nightlight on ags reload

--- home/myu/services/ags/src/widget/ControlPanel/Footer/ReloadButton.tsx
+++ home/myu/services/ags/src/widget/ControlPanel/Footer/ReloadButton.tsx
@@ -1,13 +1,18 @@
 import Hyprland from "gi://AstalHyprland";
+import NightLight from "../../../Services/NightLight";
 
 export const ReloadButton = () => {
-  const hypr = Hyprland.get_default();
   return (
     <button
       cssClasses={["reload-button"]}
       iconName={"view-refresh"}
       tooltipText={"Reload AGS"}
-      onClicked={() => hypr.dispatch("exec", "pkill gjs && ags run --gtk4")}
+      onClicked={() => {
+        // night light will not work after unless we kill the process here
+        NightLight.get_default().deactivate();
+
+        Hyprland.get_default().dispatch("exec", "pkill gjs && ags run --gtk4");
+      }}
     />
   );
 };
