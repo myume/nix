551df01d72f90c2783e1e28992ba43169f7490fd
myu <contact@memyu.com>
2025-06-25 13:59:57 -0400

    feat: add dir env and import all from dir

--- home/myu/programs/terminal/default.nix
+++ home/myu/programs/terminal/default.nix
@@ -1,10 +1,11 @@
-{
-  imports = [
-    ./nvim
-    ./zsh
-    ./tools
-    ./kitty
-    ./hyprlock
-    ./fastfetch
-  ];
+let
+  dirContents = builtins.readDir ./.;
+  dirsWithDefault = builtins.filter (
+    name:
+      dirContents.${name}
+      == "directory"
+      && builtins.pathExists (./. + "/${name}/default.nix")
+  ) (builtins.attrNames dirContents);
+in {
+  imports = map (name: ./. + "/${name}") dirsWithDefault;
 }
