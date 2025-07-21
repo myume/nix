5afd4ce97907337be3c5db426d634faa2d8317bd
myu <contact@memyu.com>
2025-07-21 00:40:46 -0400

    fix: use dp2 for monitors

--- home/myu/programs/wayland/hyprland/settings/monitor.nix
+++ home/myu/programs/wayland/hyprland/settings/monitor.nix
@@ -1,8 +1,8 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-2,1920x1080@144,auto,1"
-      "DP-4,3840x2160@144,auto,1.5"
+      # "DP-2,1920x1080@144,auto,1"
+      "DP-2,3840x2160@144,auto,1.5"
     ];
   };
 }
