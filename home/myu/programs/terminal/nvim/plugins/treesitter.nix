19f8888a7227afdaedfac0e10dd4979418bc1c36
myu <contact@memyu.com>
2025-09-02 19:21:10 -0400

    fix(nvim): remove workaround for missing typescript grammars

--- home/myu/programs/terminal/nvim/plugins/treesitter.nix
+++ home/myu/programs/terminal/nvim/plugins/treesitter.nix
@@ -5,10 +5,6 @@
       fold = true;
       autotagHtml = true;
       indent.enable = true;
-
-      grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        typescript # in language settings only tsx gets enabled, not typescript
-      ];
     };
   };
 }
