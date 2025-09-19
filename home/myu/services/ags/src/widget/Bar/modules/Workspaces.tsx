666cdf50b41b6bee364d145052de4c0c6b90096c
myu <contact@memyu.com>
2025-09-19 17:34:03 -0400

    fix(ags); populated workspaces not updating on new client

--- home/myu/services/ags/src/widget/Bar/modules/Workspaces.tsx
+++ home/myu/services/ags/src/widget/Bar/modules/Workspaces.tsx
@@ -1,16 +1,16 @@
-import { createBinding, createComputed } from "ags";
-import Hyprland from "gi://AstalHyprland";
+import { createBinding, createComputed } from "ags"
+import Hyprland from "gi://AstalHyprland"
 
 export default function Workspaces() {
-  const hypr = Hyprland.get_default();
-  const populatedWs = createBinding(hypr, "workspaces").as((workspaces) =>
-    workspaces.filter((ws) => ws.clients.length > 0).map(({ id }) => id),
-  );
+  const hypr = Hyprland.get_default()
+  const populatedWs = createBinding(hypr, "clients").as((clients) =>
+    clients.map((client) => client.workspace.id),
+  )
 
   return (
     <box cssClasses={["workspaces"]}>
       {Array.from({ length: 9 }, (_, i) => i + 1).map((wsId) => {
-        const fw = createBinding(hypr, "focusedWorkspace");
+        const fw = createBinding(hypr, "focusedWorkspace")
 
         const wsState = createComputed([populatedWs, fw], (populatedWs, fw) => [
           fw && fw.id && wsId === fw.id
@@ -18,15 +18,15 @@ export default function Workspaces() {
             : populatedWs.includes(wsId)
               ? "populated"
               : "empty",
-        ]);
+        ])
 
         return (
           <button
             cssClasses={wsState}
             onClicked={() => hypr.dispatch("workspace", wsId.toString())}
           />
-        );
+        )
       })}
     </box>
-  );
+  )
 }
