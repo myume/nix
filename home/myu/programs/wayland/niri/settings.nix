9f9745822f54b6d22565e6d115112b47b24789cf
myu <contact@memyu.com>
2025-11-04 12:57:24 -0500

    feat: add workspace keybinds to niri

--- home/myu/programs/wayland/niri/settings.nix
+++ home/myu/programs/wayland/niri/settings.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs.niri.settings = {
     prefer-no-csd = true;
 
@@ -29,53 +33,65 @@
       hot-corners.enable = false;
     };
 
-    binds = with config.lib.niri.actions; {
-      "Mod+Return".action.spawn-sh = "kitty -1";
-      "Mod+Space".action.spawn-sh = "ags toggle launcher";
-      "Mod+Shift+Slash".action = show-hotkey-overlay;
+    binds = let
+      workspaces = lib.attrsets.mergeAttrsList (lib.flatten (map (n: [
+        {
+          "Mod+${toString n}".action.focus-workspace = n;
+          "Mod+Shift+${toString n}".action.move-column-to-workspace = n;
+        }
+      ]) (lib.lists.range 1 9)));
+    in
+      with config.lib.niri.actions;
+        lib.attrsets.mergeAttrsList [
+          workspaces
+          {
+            "Mod+Return".action.spawn-sh = "kitty -1";
+            "Mod+Space".action.spawn-sh = "ags toggle launcher";
+            "Mod+Shift+Slash".action = show-hotkey-overlay;
 
-      "Mod+H".action = focus-column-or-monitor-left;
-      "Mod+L".action = focus-column-or-monitor-right;
-      "Mod+J".action = focus-window-or-monitor-down;
-      "Mod+K".action = focus-window-or-monitor-up;
+            "Mod+H".action = focus-column-or-monitor-left;
+            "Mod+L".action = focus-column-or-monitor-right;
+            "Mod+J".action = focus-window-or-workspace-down;
+            "Mod+K".action = focus-window-or-workspace-up;
 
-      "Mod+Shift+H".action = move-column-left-or-to-monitor-left;
-      "Mod+Shift+L".action = move-column-right-or-to-monitor-right;
-      "Mod+Shift+J".action = move-window-down-or-to-workspace-down;
-      "Mod+Shift+K".action = move-window-up-or-to-workspace-up;
+            "Mod+Shift+H".action = move-column-left-or-to-monitor-left;
+            "Mod+Shift+L".action = move-column-right-or-to-monitor-right;
+            "Mod+Shift+J".action = move-window-down-or-to-workspace-down;
+            "Mod+Shift+K".action = move-window-up-or-to-workspace-up;
 
-      "Ctrl+Shift+L".action = set-column-width "+10%";
-      "Ctrl+Shift+H".action = set-column-width "-10%";
-      "Ctrl+Shift+J".action = set-window-height "+10%";
-      "Ctrl+Shift+K".action = set-window-height "-10%";
+            "Ctrl+Shift+L".action = set-column-width "+10%";
+            "Ctrl+Shift+H".action = set-column-width "-10%";
+            "Ctrl+Shift+J".action = set-window-height "+10%";
+            "Ctrl+Shift+K".action = set-window-height "-10%";
 
-      "Mod+Q".action = close-window;
+            "Mod+Q".action = close-window;
 
-      "Mod+F".action = maximize-column;
-      "Mod+Shift+F".action = fullscreen-window;
+            "Mod+F".action = maximize-column;
+            "Mod+Shift+F".action = fullscreen-window;
 
-      "Mod+Tab".action = toggle-overview;
-      "Mod+O".action = toggle-overview;
+            "Mod+Tab".action = toggle-overview;
+            # "Mod+O".action = toggle-overview;
 
-      "Mod+C".action = center-column;
-      "Mod+Shift+C".action = center-visible-columns;
+            "Mod+C".action = center-column;
+            "Mod+Shift+C".action = center-visible-columns;
 
-      "Mod+V".action = toggle-window-floating;
-      "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
+            "Mod+V".action = toggle-window-floating;
+            "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
 
-      "Mod+S".action = consume-window-into-column;
-      "Mod+Shift+S".action = expel-window-from-column;
+            "Mod+S".action = consume-window-into-column;
+            "Mod+Shift+S".action = expel-window-from-column;
 
-      "XF86AudioRaiseVolume".action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
-      "XF86AudioLowerVolume".action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
-      "XF86AudioMute".action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-      "XF86AudioPlay".action = spawn-sh "playerctl play-pause";
-      "XF86AudioPrev".action = spawn-sh "playerctl previous";
-      "XF86AudioNext".action = spawn-sh "playerctl next";
+            "XF86AudioRaiseVolume".action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+            "XF86AudioLowerVolume".action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+            "XF86AudioMute".action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+            "XF86AudioPlay".action = spawn-sh "playerctl play-pause";
+            "XF86AudioPrev".action = spawn-sh "playerctl previous";
+            "XF86AudioNext".action = spawn-sh "playerctl next";
 
-      "XF86MonBrightnessUp".action = spawn-sh "brightnessctl set 2%+";
-      "XF86MonBrightnessDown".action = spawn-sh "brightnessctl set 2%-";
-    };
+            "XF86MonBrightnessUp".action = spawn-sh "brightnessctl set 2%+";
+            "XF86MonBrightnessDown".action = spawn-sh "brightnessctl set 2%-";
+          }
+        ];
 
     window-rules = [
       {
