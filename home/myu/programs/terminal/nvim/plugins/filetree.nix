3ea822debefa0953d69349eff57b540b6c5644b3
yum <contact@meyume.com>
2025-12-08 22:17:21 -0500

    feat(nvim/filetree): adjust filetree width

--- home/myu/programs/terminal/nvim/plugins/filetree.nix
+++ home/myu/programs/terminal/nvim/plugins/filetree.nix
@@ -2,6 +2,7 @@
   programs.nvf.settings.vim = {
     utility = {
       snacks-nvim.setupOpts = {
+        picker.sources.explorer.layout.layout.width = 32;
         explorer = {
           enabled = true;
         };
