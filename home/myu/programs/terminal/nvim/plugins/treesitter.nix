04417a22b3daf94694017a272148214e7d22548d
yum <contact@meyume.com>
2026-01-12 18:28:17 -0500

    fix: remove extra typescript grammar

--- home/myu/programs/terminal/nvim/plugins/treesitter.nix
+++ home/myu/programs/terminal/nvim/plugins/treesitter.nix
@@ -1,14 +1,10 @@
-{pkgs, ...}: {
+{
   programs.nvf.settings.vim = {
     treesitter = {
       enable = true;
       fold = true;
       autotagHtml = true;
       indent.enable = true;
-
-      grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        typescript # in language settings only tsx gets enabled, not typescript
-      ];
     };
   };
 }
