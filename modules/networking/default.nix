1b259b7035a363c318c21a516d299232e124cdaf
yum <contact@meyume.com>
2026-01-17 19:31:02 -0500

    misc: format networking module

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -1,11 +1,13 @@
 {hostname, ...}: {
-  networking.hostName = hostname;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = hostname;
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+    networkmanager = {
+      # Enable networking
+      enable = true;
+    };
+  };
 }
