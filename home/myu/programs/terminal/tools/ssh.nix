e9ff5cdcbc20e8bfe12a5ff6f49da59567332cf6
yum <contact@meyume.com>
2026-01-29 00:43:14 -0500

    feat: add ssh matchblock

--- home/myu/programs/terminal/tools/ssh.nix
+++ home/myu/programs/terminal/tools/ssh.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
@@ -7,6 +7,10 @@
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_ed25519";
+      };
+      "ssh.meyume.com" = {
+        hostname = "ssh.meyume.com";
+        proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h";
       };
     };
   };
