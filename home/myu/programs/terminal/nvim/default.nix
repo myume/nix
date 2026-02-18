ab5e11a6f5208f8a310828ba7a8f970e5bb536ee
yum <contact@meyume.com>
2026-02-18 15:46:27 -0500

    feat(nvim): set tabsize to 4 by default

--- home/myu/programs/terminal/nvim/default.nix
+++ home/myu/programs/terminal/nvim/default.nix
@@ -40,6 +40,10 @@ in {
           fillchars = "fold: ,foldopen:,foldclose:,foldsep: ,diff:╱,eob: ";
           diffopt = "internal,filler,closeoff,algorithm:histogram,indent-heuristic,linematch:60";
           showmode = false;
+
+          tabstop = 4;
+          shiftwidth = 4;
+          expandtab = true;
         };
       };
     };
