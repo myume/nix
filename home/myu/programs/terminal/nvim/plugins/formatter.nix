658e8960ee1f06955c9bd6fd6f5bdf4e6bfc4939
yum <contact@meyume.com>
2026-04-16 23:49:05 -0400

    feat(nvim/formatter): format only modified lines

--- home/myu/programs/terminal/nvim/plugins/formatter.nix
+++ home/myu/programs/terminal/nvim/plugins/formatter.nix
@@ -1,4 +1,6 @@
-{
+{lib, ...}: let
+  inherit (lib.generators) mkLuaInline;
+in {
   programs.nvf.settings.vim = {
     formatter.conform-nvim = {
       enable = true;
@@ -6,6 +8,27 @@
         formatters_by_ft = {
           json = ["prettier"];
         };
+        format_after_save = null;
+
+        # format only modified lines
+        # https://github.com/stevearc/conform.nvim/issues/92#issuecomment-2069915330
+        format_on_save = mkLuaInline ''
+          function()
+            local hunks = require("gitsigns").get_hunks()
+            local format = require("conform").format
+            for i = #hunks, 1, -1 do
+              local hunk = hunks[i]
+              if hunk ~= nil and hunk.type ~= "delete" then
+                local start = hunk.added.start
+                local last = start + hunk.added.count
+                -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+                local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+                local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
+                format({ range = range })
+              end
+            end
+          end
+        '';
       };
     };
   };
